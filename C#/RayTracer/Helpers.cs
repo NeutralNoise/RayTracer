eba2ab8dbdc93a1b14405b77e2e03f1f2252cec7
NeutralNoise <classicbrad24@gmail.com>
2018-09-06 08:38:37 +1000

    Added helper function file

---
+++ C#/RayTracer/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    class Helpers
+    {
+        public class Rand
+        {
+            static public float RandomBilateral()
+            {
+                return (float)ran.NextDouble();
+            }
+
+            static public float RandomUnilateral()
+            {
+                return ran.Next() / (float)System.Int32.MaxValue;
+            }
+
+            static Random ran = new Random();
+        }
+    }
+}
